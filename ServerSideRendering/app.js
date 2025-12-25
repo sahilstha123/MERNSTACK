0e3d8326627e9d35bbd099b8eea2bc63ce69e152
Sahil shrestha <sthasahil401@gmail.com>
2025-12-25 20:59:57 +0545

    first send the response

--- ServerSideRendering/app.js
+++ ServerSideRendering/app.js
@@ -2,6 +2,10 @@ import express from "express"
 const app = express()
 const PORT = 8000
 
+
+app.get("/",(req,res)=>{
+    res.status(200).send("This response is from the servers")
+})
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`https://localhost:${PORT}`)
 })
