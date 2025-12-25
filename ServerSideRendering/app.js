0b14b932a94abf23d9174075465dbb24bbe31347
Sahil shrestha <sthasahil401@gmail.com>
2025-12-25 21:56:51 +0545

    completed the navbar for ssr

--- ServerSideRendering/app.js
+++ ServerSideRendering/app.js
@@ -1,10 +1,16 @@
-import express from "express"
+const express = require("express")
+const path = require("path")
 const app = express()
 const PORT = 8000
 
+app.set("view engine","ejs")
+app.set("views","views")
+
+// serve public folder as static
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/",(req,res)=>{
-    res.status(200).send("This response is from the servers")
+    res.render("index",{title:"Homepage",message:"Hello from ejs"})
 })
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`https://localhost:${PORT}`)
