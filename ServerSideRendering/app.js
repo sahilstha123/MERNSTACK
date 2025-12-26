a8c96970e12a7e4dc10fb7efc733bfad131b183d
Sahil shrestha <sthasahil401@gmail.com>
2025-12-26 21:18:22 +0545

    completed navbar for all controllers

--- ServerSideRendering/app.js
+++ ServerSideRendering/app.js
@@ -9,8 +9,21 @@ app.set("views","views")
 // serve public folder as static
 app.use(express.static(path.join(__dirname,"public")))
 
+// Home page Controller
 app.get("/",(req,res)=>{
     res.render("index",{title:"Homepage",message:"Hello from ejs"})
+})
+
+
+// user login page controller
+app.get("/login",(req,res)=>{
+    res.render("Login",{title:"Login Page"})
+})
+
+
+// user register page controller
+app.get("/register",(req,res)=>{
+    res.status(400).render("Register",{title:"Registration Page"})
 })
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`https://localhost:${PORT}`)
