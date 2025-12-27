85ad80c349a2d1301b31f2ffefa4554d290554a8
Sahil shrestha <sthasahil401@gmail.com>
2025-12-27 17:27:06 +0545

    Writing form data in csv file

--- ServerSideRendering/app.js
+++ ServerSideRendering/app.js
@@ -2,7 +2,6 @@ const express = require("express")
 const path = require("path")
 const app = express()
 const PORT = 8000
-
 app.set("view engine","ejs")
 app.set("views","views")
 
@@ -31,10 +30,35 @@ app.get("/register",(req,res)=>{
     res.status(200).render("Register",{title:"Registration Page"})
 })
 
-app.post("/register",(req,res)=>{
-    console.log(req.body)
+// user register post 
+const fs = require("fs")
+const bcrypt = require("bcrypt")
+
+app.post("/register",async(req,res)=>{
+    const {name,email,password,confirmPassword} = req.body
+    if(password!==confirmPassword)
+    {
+        return res.status(400).send("Passwords do not match")
+    }
+    const fileName = "userList.csv"
+    
+    // create header once
+    if(!fs.existsSync(fileName)){
+        fs.writeFileSync(fileName,"Name,Email,PasswordHash\n")
+    }
+
+    // Hash Password
+    const hashPassword = await bcrypt.hash(password,10)
+
+    const row = `${name},${email},${hashPassword}\n`
+
+    fs.appendFile(fileName,row,(error)=>{
+        error?console.log(error):console.log("User registered successfully")
+    })
     res.redirect("/login")
 })
+
+
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`https://localhost:${PORT}`)
 })
