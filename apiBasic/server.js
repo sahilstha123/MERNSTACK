d6a56ec9e0e170aedbf6da64f8d80516eda8f656
Sahil shrestha <sthasahil401@gmail.com>
2025-12-29 16:28:52 +0545

    succesfully connected to postman

--- apiBasic/server.js
+++ apiBasic/server.js
@@ -2,6 +2,12 @@ const express = require("express")
 const app = express()
 const PORT = 8000
 
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"Succesfully connected to Postman"
+    })
+})
+
 app.listen(PORT, (error) => {
     error ? console.log(error) :
         console.log(`server is running at localhost:${PORT}`)
